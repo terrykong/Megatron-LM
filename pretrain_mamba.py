b1fcf5b81f7380db0314ba5453d4c1f797674030
Yi-Fu Wu <yifu.wu@gmail.com>
2025-12-02 13:51:54 -0800

    Fix merge
    Signed-off-by: Yi-Fu Wu <yifu.wu@gmail.com>

--- pretrain_mamba.py
+++ pretrain_mamba.py
@@ -264,11 +264,8 @@ def core_gpt_dataset_config_from_args(args):
         create_attention_mask=args.create_attention_mask_in_dataloader,
         object_storage_cache_path=args.object_storage_cache_path,
         mid_level_dataset_surplus=args.mid_level_dataset_surplus,
-<<<<<<< HEAD
         allow_ambiguous_pad_tokens=args.allow_ambiguous_pad_tokens,
-=======
         context_parallel_size=args.context_parallel_size,
->>>>>>> 97f6604a09 (Add sequence packing support for hybrid model)
     )
 
 
